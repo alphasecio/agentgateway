71fa4e5d1d117c4b800aee466334703e47d65cea
alphasec <95212748+alphasecio@users.noreply.github.com>
2026-03-30 17:26:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ ! -f /data/config.yaml ]; then
-  cp /app/config.yaml.default /data/config.yaml
+  cp /data/config.yaml.default /data/config.yaml
 fi
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
